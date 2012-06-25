38eac9c40e279332e0a82e64c9730c35ca8c74ac
James Booth <boothj5@gmail.com>
2012-06-25 20:09:12 +0100

    Simplified byte_to_str

--- common.c
+++ common.c
@@ -6,14 +6,14 @@ char *byte_to_str(BYTE byte)
 {
     char *str = malloc(9 * sizeof(char));
 
-    str[0] = (( byte & 0x80 ) != 0) ? '1' : '0';
-    str[1] = (( byte & 0x40 ) != 0) ? '1' : '0';
-    str[2] = (( byte & 0x20 ) != 0) ? '1' : '0';
-    str[3] = (( byte & 0x10 ) != 0) ? '1' : '0';
-    str[4] = (( byte & 0x08 ) != 0) ? '1' : '0';
-    str[5] = (( byte & 0x04 ) != 0) ? '1' : '0';
-    str[6] = (( byte & 0x02 ) != 0) ? '1' : '0';
-    str[7] = (( byte & 0x01 ) != 0) ? '1' : '0';
+    str[0] = (byte & 0x80) ? '1' : '0';
+    str[1] = (byte & 0x40) ? '1' : '0';
+    str[2] = (byte & 0x20) ? '1' : '0';
+    str[3] = (byte & 0x10) ? '1' : '0';
+    str[4] = (byte & 0x08) ? '1' : '0';
+    str[5] = (byte & 0x04) ? '1' : '0';
+    str[6] = (byte & 0x02) ? '1' : '0';
+    str[7] = (byte & 0x01) ? '1' : '0';
     str[8] = '\0';
 
     return str;
