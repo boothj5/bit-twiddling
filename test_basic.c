fba8bf0a39b02c85130ea04aaf1c42c871aee044
James Booth <boothj5@gmail.com>
2012-06-28 01:10:36 +0100

    Test set and zeroes mask

--- test_basic.c
+++ test_basic.c
@@ -84,6 +84,78 @@ void test_isolate_rightmost_set(void)
         "10100000"))));
 }
 
+void test_isolate_rightmost_unset(void)
+{
+    assert_string_equals(
+        "00000001", byte_to_str(isolate_rightmost_unset(str_to_byte(
+        "00000000"))));
+    assert_string_equals(
+        "00000010", byte_to_str(isolate_rightmost_unset(str_to_byte(
+        "00000001"))));
+    assert_string_equals(
+        "00000010", byte_to_str(isolate_rightmost_unset(str_to_byte(
+        "10000001"))));
+    assert_string_equals(
+        "00000001", byte_to_str(isolate_rightmost_unset(str_to_byte(
+        "00000100"))));
+    assert_string_equals(
+        "00000001", byte_to_str(isolate_rightmost_unset(str_to_byte(
+        "11011000"))));
+    assert_string_equals(
+        "00000100", byte_to_str(isolate_rightmost_unset(str_to_byte(
+        "00010011"))));
+    assert_string_equals(
+        "00001000", byte_to_str(isolate_rightmost_unset(str_to_byte(
+        "11000111"))));
+    assert_string_equals(
+        "01000000", byte_to_str(isolate_rightmost_unset(str_to_byte(
+        "10111111"))));
+    assert_string_equals(
+        "00010000", byte_to_str(isolate_rightmost_unset(str_to_byte(
+        "10101111"))));
+}
+
+void test_identify_trailing_zeroes(void)
+{
+    assert_string_equals(
+        "00000000", byte_to_str(identify_trailing_zeroes(str_to_byte(
+        "11111111"))));
+    assert_string_equals(
+        "00000001", byte_to_str(identify_trailing_zeroes(str_to_byte(
+        "11111110"))));
+    assert_string_equals(
+        "00000011", byte_to_str(identify_trailing_zeroes(str_to_byte(
+        "11111100"))));
+    assert_string_equals(
+        "00000011", byte_to_str(identify_trailing_zeroes(str_to_byte(
+        "00000100"))));
+    assert_string_equals(
+        "00000111", byte_to_str(identify_trailing_zeroes(str_to_byte(
+        "00101000"))));
+    assert_string_equals(
+        "01111111", byte_to_str(identify_trailing_zeroes(str_to_byte(
+        "10000000"))));
+}
+
+void test_identify_rightmost_set_and_trailing_zeroes(void)
+{
+    assert_string_equals(
+        "11111111", byte_to_str(identify_rightmost_set_and_trailing_unset(str_to_byte(
+        "00000000"))));
+    assert_string_equals(
+        "00000001", byte_to_str(identify_rightmost_set_and_trailing_unset(str_to_byte(
+        "11111111"))));
+    assert_string_equals(
+        "00011111", byte_to_str(identify_rightmost_set_and_trailing_unset(str_to_byte(
+        "11110000"))));
+    assert_string_equals(
+        "00011111", byte_to_str(identify_rightmost_set_and_trailing_unset(str_to_byte(
+        "10010000"))));
+    assert_string_equals(
+        "00111111", byte_to_str(identify_rightmost_set_and_trailing_unset(str_to_byte(
+        "10100000"))));
+}
+
 void register_basic_tests(void)
 {
     TEST_MODULE("basic tests");
@@ -92,4 +164,7 @@ void register_basic_tests(void)
     TEST(test_all_bits_same);
     TEST(test_not_all_bits_same);
     TEST(test_isolate_rightmost_set);
+    TEST(test_isolate_rightmost_unset);
+    TEST(test_identify_trailing_zeroes);
+    TEST(test_identify_rightmost_set_and_trailing_zeroes);
 }
